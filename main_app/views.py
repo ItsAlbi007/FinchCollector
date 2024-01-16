464925f9869ba1d6a63e9b5af310fa8cafc0e978
albi <albijon.re@gmail.com>
2024-01-15 21:16:01 -0500

    adds finch and index view for finch

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 
+from .models import Finch
+
 # add finch list below 
 # views.py
 
 # Add this cats list below the imports
-finch = [
-  {'name': 'Haemorhous purpureus', 'breed': 'Fringillidae', 'description': 'Home Finch', 'age': 1},
-  {'name': 'Red avadavat', 'breed': 'Estrildidae', 'description': 'Strawberry Finch', 'age': 2},
-  {'name': 'Bicheno finch', 'breed': 'Taeniopygia bichenovii', 'description': 'Owl Finch', 'age': 0},  
-]
+# this was to build the intial view - now we have finch in the db
+#finch = [
+#  {'name': 'Haemorhous purpureus', 'breed': 'Fringillidae', 'description': 'Home Finch', 'age': 1},
+#  {'name': 'Red avadavat', 'breed': 'Estrildidae', 'description': 'Strawberry Finch', 'age': 2},
+#  {'name': 'Bicheno finch', 'breed': 'Taeniopygia bichenovi', 'description': 'Owl Finch', 'age': 0},  
+#]
 
 # Create your views here.
 
@@ -22,4 +25,10 @@ def about(request):
 
 # index view - shows all finch
 def finch_index(request):
+  # collect out objects from teh database
+  finch = Finch.objects.all()
+  #print(finch)
+  # for Finch in finch:
+  #  print(finch)
+
   return render(request, 'finch/index.html', { 'finch': finch })
