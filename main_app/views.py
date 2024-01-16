bc97f754c1ce239eddab4ddf67d99f5f4482cbf3
albi <albijon.re@gmail.com>
2024-01-15 19:57:42 -0500

    add index finch placeholder

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+# add finch list below 
+# views.py
+
+# Add this cats list below the imports
+finch = [
+  {'name': 'Haemorhous purpureus', 'breed': 'Fringillidae', 'description': 'Home Finch', 'age': 1},
+  {'name': 'Red avadavat', 'breed': 'Estrildidae', 'description': 'Strawberry Finch', 'age': 2},
+  {'name': 'Bicheno finch', 'breed': 'Taeniopygia bichenovii', 'description': 'Owl Finch', 'age': 0},  
+]
+
 # Create your views here.
 
 # define home view here - '/'
@@ -9,3 +19,7 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+# index view - shows all finch
+def finch_index(request):
+  return render(request, 'finch/index.html', { 'finch': finch })
