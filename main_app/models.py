d78954cb39b209ba7f37ca42037b5e7ab73b8691
albi <albijon.re@gmail.com>
2024-01-18 15:32:56 -0500

    django authentication

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 from datetime import date
+from django.contrib.auth.models import User
 
 MEALS = (
     # this is a tuple with multiple tuples
@@ -26,6 +27,8 @@ class Finch(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
   def __str__(self):
     return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Finch, Toy, Photo
 from .forms import FeedingForm
 
@@ -19,35 +23,38 @@ def home(request):
 def about(request):
   return render(request, 'about.html')
 
-# index view - shows all finch
+@login_required
 def finch_index(request):
-  finch = Finch.objects.all()
-
+  finch = Finch.objects.filter(user=request.user)
   return render(request, 'finch/index.html', { 'finch': finch })
 
-# detail view - shows finch at '.finch/:id
+@login_required
 def finch_detail(request, finch_id):
   finch = Finch.objects.get(id=finch_id)
   feeding_form = FeedingForm()
   return render(request, 'finch/detail.html', { 'finch': finch, 'feeding_form': feeding_form })
 
 # inherit from CBV
-class FinchCreate(CreateView):
+class FinchCreate(LoginRequiredMixin, CreateView):
   model = Finch
-  fields = '__all__'
+  fields = ['name', 'breed', 'description', 'age']
    # success_url = '/finch/{finch_id}'
   
-class FinchUpdate(UpdateView):
+  def form_valid(self, form):
+     form.instance.user = self.request.user
+     return super().form_valid(form)
+  
+class FinchUpdate(LoginRequiredMixin, UpdateView):
     model = Finch
     fields = ['breed', 'description', 'age']
 
 # Delete View - extends DeleteView
-class FinchDelete(DeleteView):
+class FinchDelete(LoginRequiredMixin, DeleteView):
     model = Finch
 
     success_url = '/finch'
 
-# FEEDING AND RELATIONSHIP VIEW FUNCTIONS
+@login_required
 def add_feeding(request, finch_id):
     form = FeedingForm(request.POST)
     if form.is_valid():
@@ -56,17 +63,17 @@ def add_feeding(request, finch_id):
         new_feeding.save()
     return redirect('detail', finch_id=finch_id)
 
-class ToyList(ListView):
+class ToyList(LoginRequiredMixin, ListView):
     model = Toy
     template_name = 'toys/index.html'
 
 # ToyDetail
-class ToyDetail(DetailView):
+class ToyDetail(LoginRequiredMixin, DetailView):
     model = Toy
     template_name = 'toys/detail.html'
 
 # ToyCreate
-class ToyCreate(CreateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = ['name', 'color']
 
@@ -74,15 +81,16 @@ class ToyCreate(CreateView):
         return super().form_valid(form)
 
 # ToyUpdate
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
     
 # ToyDelete
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys'
 
+@login_required
 def add_photo(request, finch_id):
    photo_file = request.FILES.get('photo-file', None)
    if photo_file:
@@ -97,3 +105,18 @@ def add_photo(request, finch_id):
          print('An error occurred uploading file to S3')
          print(e)
       return redirect('detail', finch_id=finch_id)   
+   
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect ('index')
+      else:
+        error_message = 'Invalid sign up - try again'
+        
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
